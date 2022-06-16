f5de8e7df659edd3da6ad8fb0f2f575f51b448a8
TinyBossofaMilk <ppcctan@gmail.com>
2022-06-16 01:58:03 -0700

    worked on js table population

--- book.js
+++ book.js
@@ -1,13 +1,13 @@
-let myLibrary = new Array();
+let myLibrary = [];
 
 function Book()
 {
     // name, author, pages, hasRead
 }
 
-function Book(name, author, pages, hasRead)
+function Book(title, author, pages, hasRead)
 {
-    this.name = name;
+    this.title = title;
     this.author = author;
     this.pages = pages;
     this.hasRead = hasRead;
@@ -17,13 +17,65 @@ function Book(name, author, pages, hasRead)
 Book.prototype.info = function()
 {
     console.log(this);
-    return `${this.name} by ${this.author}, ${this.pages} pages, ${ this.hasRead ? "read" : "not read yet"}`;
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${ this.hasRead ? "read" : "not read yet"}`;
 }
 
 theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+myLibrary.push(theHobbit);
 console.log(theHobbit.info());
 
+
+
+const table = document.querySelector(".card-table");
 function addBookToLibrary()
 {
-    
+    for(const book of myLibrary)
+    {
+        const card = document.createElement("div");
+        card.className = ".card";
+
+        const title = document.createElement("div");
+        title.textContent = book.title;
+        title.className = ".title";
+
+        const author = document.createElement("div");
+        author.textContent = book.author;
+        author.className = ".author";
+
+        card.appendChild(title);
+        card.appendChild(author);
+
+        table.appendChild(card);
+    }
 }
+
+
+addBookToLibrary();
+
+/*
+If you haven’t already, set up your project with skeleton HTML/CSS and JS files.
+All of your book objects are going to be stored in a simple array, so add a function to the script (not the constructor) that can take user’s input and store the new book objects into an array. Your code should look something like this:
+
+let myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+Write a function that loops through the array and displays each book on the page. You can display them in some sort of table, or each on their own “card”. 
+It might help for now to manually add a few books to your array so you can see the display.
+
+Add a “NEW BOOK” button that brings up a form allowing users to input the details for the new book: 
+author, title, number of pages, whether it’s been read and anything else you might want.
+
+Add a button on each book’s display to remove the book from the library.
+
+You will need to associate your DOM elements with the actual book objects in some way. 
+One easy solution is giving them a data-attribute that corresponds to the index of the library array.
+
+Add a button on each book’s display to change its read status.
+To facilitate this you will want to create the function that toggles a book’s read status on your Book prototype instance.
+*/
